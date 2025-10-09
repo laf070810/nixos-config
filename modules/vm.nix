9d197bb180dbf0e73f87bffcc18bc17b7c8db8da
laf070810 <laf070810@163.com>
2025-10-09 21:53:09 +0800

    fix OVMF config error

--- modules/vm.nix
+++ modules/vm.nix
@@ -8,6 +8,5 @@
 {
   virtualisation.libvirtd.enable = true;
   virtualisation.libvirtd.qemu.swtpm.enable = true;
-  virtualisation.libvirtd.qemu.ovmf.packages = [ pkgs.OVMFFull.fd ];
   programs.virt-manager.enable = true;
 }
