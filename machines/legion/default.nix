88181ac12a148d7f35c7fe53d28bbeff9c62198d
laf070810 <laf070810@163.com>
2026-03-29 03:19:05 +0800

    enable nix flake globally

--- machines/legion/default.nix
+++ machines/legion/default.nix
@@ -265,6 +265,11 @@
     }
   ];
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
